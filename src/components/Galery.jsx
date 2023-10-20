3a85258e571b9a015815da830b53651af9f3b9b9
Robin98123 <robinson1998sena@gmail.com>
2023-10-20 15:22:18 -0500

    se sube links de galeria

--- src/components/Galery.jsx
+++ src/components/Galery.jsx
@@ -6,22 +6,24 @@ const Galery = () => {
   return (
     <div className='grid-galery' id="Galery">
 
+        {/* soundwave */}
         <div className='image1'> 
             <div className='background-container'>
                 <div className='background'>
                     <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <p>Diseño landing page UX/UI sonudWave plataforma y libreria musical ondemand</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://www.behance.net/gallery/182748515/Sound-Wave-landing-page" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/182748515/Sound-Wave-landing-page" target='blank'><span className='icon-behance'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* sajubcha */}
 
         <div className='image2'>
         <div className='background-container'>
@@ -30,112 +32,121 @@ const Galery = () => {
                     <p>El té Sabujcha es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable ( MVP )</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://sabujcha-tea-maquetacion.vercel.app/" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://github.com/Robin98123/Sabujcha-Tea-Maquetacion" target='blank'><span className='icon-github'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* one piece */}
 
         <div className='image3'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
+                    <h1>One Piece fandom</h1>
                     <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://onepicefandom.vercel.app/" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/59151623/One-piece-fandon-website" target='blank'><span className='icon-behance'></span></a>
+                        <a href="https://github.com/Robin98123/onepicefandom" target='blank'><span className='icon-github'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* masyá tienda de mascotas */}
 
         <div className='image4'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <h1>Masyá tienda de mascotas</h1>
+                    <p>Web site shop para la tienda de mascotas masyá desarrollado en React, donde se implentaron elementos de ux/ui y desarrollo código</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://masy-tienda-de-mascotas.vercel.app/" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/163224947/Knowmeet-proyect-mockup" target='blank'><span className='icon-behance'></span></a>
+                        <a href="https://github.com/Robin98123/Masy-tienda-de-mascotas" target='blank'><span className='icon-github'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* knowmeet */}
 
         <div className='image5'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <h1>KnowMeet</h1>
+                    <p>Diseño web UX/UI aplicativo web knowMeet dasboard, para control y manejo de tareas, asinación de curso y seguimiento de los mismos</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://www.behance.net/gallery/163224947/Knowmeet-proyect-mockup" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/163224947/Knowmeet-proyect-mockup" target='blank'><span className='icon-behance'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* barrionline */}
 
         <div className='image6'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <h1>barrionline</h1>
+                    <p>Diseño web del aplicativo startup barrionle, platforma web que permite conectar tenderos de la región directamente con sus consumidores</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://www.behance.net/gallery/150279939/Barrionline-Website-Design-Home-page" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/150279939/Barrionline-Website-Design-Home-page" target='blank'><span className='icon-behance'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {/* fundacion  */}
 
         <div className='image7'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <h1>Fundación seillas Dashboard</h1>
+                    <p>Demo (MVP) del dashboard para la fundación semillas, enfocándose primordialmente en la salud emocional de los estudiantes</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://robin98123.github.io/fundaci-n-semillas-dashboard-/" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/146764285/Fundacion-semillas-Dashboard" target='blank'><span className='icon-behance'></span></a>
+                        <a href="https://github.com/Robin98123/fundaci-n-semillas-dashboard-" target='blank'><span className='icon-github'></span></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
+        {/* servitec */}
+
         <div className='image8'>
         <div className='background-container'>
                 <div className='background'>
-                    <h1>Sound Wave</h1>
-                    <p>One Piece fandom es un proyecto de diseño con tecnologías HTML y CSS, producto mínimo viable (MVP)</p>
+                    <h1>EC servitec</h1>
+                    <p>Web site tienda de etiquetadoras, sellos y cosedoras industriales colombiana dedicada a la venta y mantenimeinto de estos equipos</p>
 
                     <div className='links-deploy-container'>
-                        <a href="#" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
+                        <a href="https://www.ecservitec.com/" className='btn-visitarsitio' target='blank'>Visitar sitio<span class="icon-flecha"></span></a>
                         <div className='links-deploy'>
-                        <a href="https://www.behance.net/ryrinconmultimedia" target='blank'><span className='icon-behance'></span></a>
-                        <a href="https://github.com/Robin98123" target='blank'><span className='icon-github'></span></a>
+                        <a href="https://www.behance.net/gallery/178362993/EC-servitec-Landing-page-website" target='blank'><span className='icon-behance'></span></a>
+                        <a href="https://github.com/Nisanech/EcServitec" target='blank'><span className='icon-github'></span></a>
                         </div>
                     </div>
                 </div>
